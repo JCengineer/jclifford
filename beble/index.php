345b8119742dc52a693e624d89238d5b4b445c39
James Clifford <engineer@jclifford.ie>
2016-01-05 00:45:48 +0000

    typo in website, new beble site

---
+++ beble/index.php
@@ -0,0 +1,143 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+  <div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/01.png');">
+    <div id="pageintro" class="hoc clear"> 
+      <article class="introtxt">
+        <h2 class="heading">Small-Space living is in need of <strong>BEBLE</strong></h2>
+        <p>Bed, table, couch and free space all in one.</p>
+        <footer>
+          <ul class="nospace inline pushright">
+            <li><a class="btn" href="#">BUY</a></li>
+            <li><a class="btn inverse" href="#more">Read More...</a></li>
+          </ul>
+        </footer>
+      </article>
+    </div>
+  </div>
+  <div class="wrapper row3">
+    <section id="more">
+      <main class="hoc container clear"> 
+        <!-- main body -->
+        <h2 class="font-x3 uppercase btmspace-80">Multifunctional <a href="#">BED / TABLE</a></h2>
+        
+        <ul class="nospace group">
+          <li class="one_quarter first">
+            <div class="inspace-30 borderedbox">
+              <h6 class="heading">Wouldn't it be nice to have the luxury of 3 great rooms in one small space?</h6>
+              <p>Smarter use of floor and ceiling space means exactly that. Your bed, couch and table are mutually exclusive. 
+                You never use one while using the other...</p>
+            </div>
+          </li>
+          <li class="one_quarter">
+            <article><a href="#"><img class="btmspace-30" src="images/demo/320x220.png" alt=""></a>
+              <h6 class="heading">Smart bed</h6>
+              <p>When a great night's sleep calls, the bed needs no attention. Just tuck in.</p>
+            </article>
+          </li>
+          <li class="one_quarter">
+            <article><a href="#"><img class="btmspace-30" src="images/demo/320x220.png" alt=""></a>
+              <h6 class="heading">Smart table</h6>
+              <p>A dinner party or a home office needs a large clean table quickly!</p>
+            </article>
+          </li>
+          <li class="one_quarter">
+            <article><a href="#"><img class="btmspace-30" src="images/demo/320x220.png" alt=""></a>
+              <h6 class="heading">Couch / free space</h6>
+              <p>Combining the headboard and the stools makes a great and comfy couch, or just leave space to breathe.</p>
+            </article>
+          </li>
+        </ul>
+            <!-- / main body -->
+        <div class="clear"></div>
+      </main>
+    </section>
+  </div>
+  <div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/02.png');">
+    <section class="hoc container clear"> 
+      <h2 class="font-x3 uppercase btmspace-80 underlined">Engineered solution</h2>
+      <div class="group center btmspace-50">
+        <figure class="one_quarter first"><img src="images/demo/320x220.png" alt="">
+          <figcaption>
+            <p>3D printed steel parts</p>
+            <p>No costly parts to make, each section is custom built using 3d printing techniques.</p>
+          </figcaption>
+        </figure>
+        <figure class="one_quarter"><img src="images/demo/320x220.png" alt="">
+          <figcaption>
+            <p>Wooden / Steel frame design</p>
+            <p>For functional and stylish finishes, wooden or steel frames can be choosen.</p>
+          </figcaption>
+        </figure>
+        <figure class="one_quarter"><img src="images/demo/320x220.png" alt="">
+          <figcaption>
+            <p>Fully automated transitions</p>
+            <p>The bed, table and free space can be called upon at the click of a button.</p>
+          </figcaption>
+        </figure>
+        <figure class="one_quarter"><img src="images/demo/320x220.png" alt="">
+          <figcaption>
+            <p>High Tolerance Weighting System</p>
+            <p>Counterweights ensure the bed moves with ease and protects you and the bed even during power outages.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <p><a class="btn medium" href="#">Register your interest</a></p>
+    </section>
+  </div>
+  <div class="wrapper row4">
+    <footer id="footer" class="hoc clear"> 
+      <div class="one_third first">
+        <h6 class="title">Aliquet iaculis venenatis</h6>
+        <ul class="nospace linklist contact">
+          <li><i class="fa fa-map-marker"></i>
+            <address>
+            Street Name &amp; Number, Town, Postcode/Zip
+            </address>
+          </li>
+          <li><i class="fa fa-phone"></i> +00 (123) 456 7890<br>
+            +00 (123) 456 7890</li>
+          <li><i class="fa fa-fax"></i> +00 (123) 456 7890</li>
+          <li><i class="fa fa-envelope-o"></i> info@domain.com</li>
+        </ul>
+      </div>
+      <div class="one_third">
+        <h6 class="title">aliquam elit enim dictum</h6>
+        <ul class="nospace linklist">
+          <li><a href="#">hendrerit risus nulla consectetur</a></li>
+          <li><a href="#">sapien a tempor neque faucibus vitae</a></li>
+          <li><a href="#">vestibulum nisi sed orci condimentum</a></li>
+          <li><a href="#">faucibus ligula facilisis phasellus</a></li>
+          <li><a href="#">venenatis mauris vel varius tincidunt</a></li>
+        </ul>
+      </div>
+      <div class="one_third">
+        <h6 class="title">egestas diam sollicitudin</h6>
+        <ul class="nospace linklist">
+          <li><a href="#">non fusce quis dictum vitae accumsan</a></li>
+          <li><a href="#">erat in hac habitasse platea dictumst</a></li>
+          <li><a href="#">sed mi arcu mattis gravida feugiat</a></li>
+          <li><a href="#">aliquam nunc etiam neque lobortis</a></li>
+          <li><a href="#">tristique integer nunc eget dictum</a></li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+  <div class="wrapper row5">
+    <div id="copyright" class="hoc clear"> 
+      <p class="fl_left">Copyright &copy; 2015 - All Rights Reserved - <a href="jclifford.ie">James Clifford</a></p>
+      <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    </div>
+  </div>
+  <a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+  <!-- JAVASCRIPTS -->
+  <script src="layout/scripts/jquery.min.js"></script>
+  <script src="layout/scripts/jquery.backtotop.js"></script>
+  <script src="layout/scripts/jquery.mobilemenu.js"></script>
+  <!-- IE9 Placeholder Support -->
+  <script src="layout/scripts/jquery.placeholder.min.js"></script>
+  <!-- / IE9 Placeholder Support -->
+</body>
+</html>
--- php/header.php
+++ php/header.php
@@ -2,7 +2,7 @@
 	<div>
 		<h1>James Clifford - Engineering &amp; Development Services</h1>
 		<h2>My online CV, portfolio, idea pad and blog...</h2>
-		<h3>I build interative websites.</h3>
+		<h3>I build interactive websites.</h3>
 		<h4>I have worked in tech and start ups for over 8 years.</h4>
 		<br>
 		<!--a href="tour.php" class="callToAction">Take a tour</a>
